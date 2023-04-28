3d127343c5636fe4c8be34984ce51c0a374887fb
BARBAPP <98393516+BARBAPP@users.noreply.github.com>
2023-04-29 03:30:51 +0530

    Update Navigation.tsx

--- components/NewNavbar/Navigation.tsx
+++ components/NewNavbar/Navigation.tsx
@@ -6,7 +6,7 @@ const Navigation = () => {
         <>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
+
 <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet"></link>
           </Head>
         <ul>
